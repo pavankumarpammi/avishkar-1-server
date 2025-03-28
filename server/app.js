1359970c24c4e30583c8da754d7b0098109967e2
Pavankumar Pammi <pammimanikantapavankumar@gmail.com>
2025-03-28 17:55:32 +0530

    v1

--- server/app.js
+++ server/app.js
@@ -6,10 +6,9 @@ import courseRoutes from './routes/courseRoutes.js';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+  origin: process.env.CLIENT_URL,
+  credentials: true,
 }));
-
 app.use(express.json());
 app.use(cookieParser());
 
