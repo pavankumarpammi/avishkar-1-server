cb3106b66af686bee9c446b4eeb6b46c5236d56d
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-29 15:31:02 +0530

    Deployment - Update MongoDB connection URI format for consistency ✅

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const MONGO_URI = process.env.MONGO_URI
+        const MONGO_URI =`mongodb+srv://${process.env.MONGO_URI}`
         await mongoose.connect(MONGO_URI);        
       console.log('MongoDB Connected');
     } catch (error) {
