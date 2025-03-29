6e905c9c946093d5b63f13bbf6d0de37da4350ac
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-29 21:43:37 +0530

    cvbrhtn

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? process.env.FRONTEND_URL || "https://avishkar-1-server-theta.vercel.app/" 
+        ? process.env.FRONTEND_URL || "https://avishkar-1-server-theta.vercel.app" 
         : "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
