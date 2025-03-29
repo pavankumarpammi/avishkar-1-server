7a4b4f5932c71a80966c309a20e3a16b77c2e51e
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-29 17:03:48 +0530

    Deployment - Enhance MongoDB connection options for improved compatibility

--- server/database/db.js
+++ server/database/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const MONGO_URI =process.env.MONGO_URI
-        await mongoose.connect(MONGO_URI);
+        const MONGO_URI = process.env.MONGO_URI
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
       console.log('MongoDB Connected');
     } catch (error) {
         console.log("error occured", error); 
