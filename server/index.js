5fcad45c3078e11dd89b3ca9d5ed084f1335abf2
sraawan-sai <sraawan.sai@gmail.com>
2025-04-03 12:22:24 +0000

    add "patch" for cross

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cors({
         ? process.env.FRONTEND_URL || "https://avishkar-1-server-theta.vercel.app/"
         : "http://localhost:5173",
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE', 'OPTIONS',], 
   allowedHeaders: ['Content-Type', 'Authorization', 'X-HTTP-Method-Override'],
 }));
 
